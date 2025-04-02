13989236336da7be2220c344ee7a0cbdcc104d0d
Daniel Kim <72123261+agent-platypus@users.noreply.github.com>
2025-04-02 14:14:48 -0700

    Add files via upload

---
+++ datafiles_download.tcl
@@ -0,0 +1,10 @@
+connect 
+
+dow -data U:/ENSC452_datafiles/StartScreen.data 0x018D2008
+dow -data U:/ENSC452_datafiles/StartScreenStartGame.data 0x020BB00C
+dow -data U:/ENSC452_datafiles/StartScreenOptions.data 0x028A4010
+dow -data U:/ENSC452_datafiles/OptionScreen.data 0x0308D014
+dow -data U:/ENSC452_datafiles/Easy_text.data 0x03876018
+dow -data U:/ENSC452_datafiles/Medium_text.data 0x03896018
+dow -data U:/ENSC452_datafiles/Victory_Text.data 0x05006018
+dow -data U:/ENSC452_datafiles/Defeat_Text.data 0x03926018
